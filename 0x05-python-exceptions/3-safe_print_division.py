30d40528d5d76733e8923d2238c62b2e9af158a3
Nazasophie <chinazasundaygoodness@gmail.com>
2022-12-19 15:51:31 -0800

    correction

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-[2;2R[>77;30601;0c]10;rgb:bfbf/bfbf/bfbf]11;rgb:0000/0000/0000def safe_print_division(a, b):
+def safe_print_division(a, b):
     """Returns the division of a by b."""
     try:
         div = a / b
