42fbe72bf0a0bea100d9ceded203832c97c96341
Nazasophie <chinazasundaygoodness@gmail.com>
2023-03-31 05:29:55 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This scripts takes in a URL, sends a request to that URL, and displays the size of the body of the response
-# The size must be displayed in bytes
-# usage: ./0-body_size.sh URL
-curl -sI "$1" | grep content-Length | awk '{print $2}'
+# send a request to an URL with curl, and displays the size of the body of the response
+curl -s "$1" | wc -c
